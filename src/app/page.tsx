bf454be26c07d0600b9f4b3b50b966c1a6278f5a
HA-1-N <hoanganh02062001@gmail.com>
2024-01-22 15:38:30 +0700

    feat(home): build slider with carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 'use client';
+import HomeSlider from '@/ui/home/home-slider/HomeSlider';
 import HotProduct from '@/ui/home/hot-product/HotProduct';
 
 export default function Home() {
   return (
     <>
       <main>
+        <HomeSlider />
         <HotProduct />
       </main>
     </>
